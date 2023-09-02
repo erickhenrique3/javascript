18b731533596196db7159c1f1d35344344d8bfaa
erickhenrique3 <erick.2henriquehtp@gmail.com>
2023-09-02 15:15:15 -0300

    sistemas de multas feito izzy!1

---
+++ EXERCICIOS/ex005/ex005.js
@@ -0,0 +1,8 @@
+var vel = 55.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if(vel > 60){// CONDIÇÃO SIMPLES :)
+    console.log(`Você ultrapassou a velocidade.MULTADO1`)
+}
+
+console.log(`Dirija sempre com o cinto de segurança!`)
---
+++ EXERCICIOS/ex006/ex006.js
@@ -0,0 +1,7 @@
+var país = 'Brazil'
+console.log(`Vivendo em ${país}!`)
+if(país == 'Brazil'){
+    console.log(`Você é Brasileiro!`)
+}else{
+    console.log(`Você é americano:(`)
+}
