0ba1ee43870c4245238bf0bb1805c428574bb27f
erickhenrique3 <erick.2henriquehtp@gmail.com>
2023-09-28 21:24:53 -0300

    vapooooooooo

---
+++ EXERCICIOS/ex010/index.js
@@ -0,0 +1,14 @@
+
+/*var c = 1
+ while(c <= 5){
+    console.log(c)
+    c += 1
+ }
+*/
+ //////////////////////
+   console.log('Vai começar....')
+ for(var c = 1 ;c <= 3 ;c += 1  ){
+      console.log(c)
+ }
+ console.log('Ação!!')
+
